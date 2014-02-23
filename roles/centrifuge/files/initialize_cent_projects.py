bbe6c43149ed2c21023e1b8338ae0153df4a0b2f
Dave O <davidobrite@gmail.com>
2014-02-22 18:48:39 -0800

    made this idempotent

--- roles/centrifuge/files/initialize_cent_projects.py
+++ roles/centrifuge/files/initialize_cent_projects.py
@@ -8,16 +8,33 @@ def load_config():
         return json.loads(f.read())
 
 
-def create_project(name, config):
-    client = Client("http://localhost:8000/api", "_", config['api_secret'])
-    result, error = client.send("project_create", {"name": name,
-                                                   "display_name": name, })
+def project_exists(admin_client):
+    result, error = admin_client.send("project_list", {})
+    if not result['error']:
+        return 'wwc' in [project['name'] for project in result['body']]
+
+    exit(1)  # connection error or otherwise
+
+
+def get_project(name, admin_client):
+    result, error = admin_client.send("project_list", {})
+    if not result['error']:
+        return [(project['_id'], project['secret_key'])
+                for project in result['body']
+                if project['name'] == 'wwc'][0]
+
+    exit(1)  # connection error or otherwise
+
+
+def create_project(name, admin_client):
+    params = {"name": name, "display_name": name, }
+    result, error = admin_client.send("project_create", params)
     if not result['error']:
         project_id = result['body']['_id']
         secret_key = result['body']['secret_key']
         return project_id, secret_key
 
-    return None, None
+    exit(1)  # connection error or otherwise
 
 
 def create_namespace(name, project_id, secret_key):
@@ -31,13 +48,17 @@ def create_namespace(name, project_id, secret_key):
 
 def main():
     config = load_config()
-    project_id, secret_key = create_project('wwc', config)
-    if project_id and secret_key:
-        print(project_id)
-        print(secret_key)
+    admin_client = Client("http://localhost:8000/api",
+                          "_",
+                          config['api_secret'])
+    if project_exists(admin_client):
+        project_id, secret_key = get_project('wwc', admin_client)
+    else:
+        project_id, secret_key = create_project('wwc', admin_client)
         create_namespace('wwc', project_id, secret_key)
-        exit(0)
-    exit(1) # signal to ansible
+
+    print(project_id)  # for ansible's benefit
+    print(secret_key)  # for ansible's benefit
 
 if __name__ == "__main__":
     main()
